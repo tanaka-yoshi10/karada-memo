66b589f97bb79f13ce5ad65446612d409b2c90eb
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2018-09-10 22:29:53 +0900

    refactor: seedの重複を排除 #37

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,31 @@
-# 環境ごとにseedを分ける
-load(Rails.root.join('db', 'seeds', "#{Rails.env.downcase}.rb"))
+# frozen_string_literal: true
+
+def clear_model_classes(model_classes)
+  model_classes.each { |clazz| clazz.try(:destroy_all) }
+  puts "clear: #{model_classes.join(', ')}"
+end
+
+def puts_user_info(user)
+  puts user.inspect
+end
+
+puts '-- seed'
+clear_model_classes [User, Body, Family, Note]
+
+test1 = FactoryBot.create(:user, email: 'demo1@example.com', nickname: 'デモユーザー1')
+FactoryBot.create(:body, :with_notes, family: test1.family, note_count: 0)
+FactoryBot.create(:body, :with_notes, family: test1.family, note_count: 3)
+FactoryBot.create(:body, :with_notes, family: test1.family, note_count: 20)
+puts_user_info test1
+
+test2 = FactoryBot.create(:user, email: 'demo2@example.com', nickname: 'デモユーザー2')
+FactoryBot.create(:body, :with_notes, family: test2.family, note_count: 10)
+puts_user_info test2
+
+admin = FactoryBot.create(:user, email: 'admin@example.com', nickname: '管理者')
+admin.add_role :admin
+puts_user_info admin
+
+50.times do
+  puts_user_info FactoryBot.create(:user)
+end
